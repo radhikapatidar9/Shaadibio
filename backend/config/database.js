452c0f5f9f8704344f568f6df4f1ef821b5e246e
RADHIKA PATIDAR <radhikapatidar990@gmail.com>
2026-03-10 17:26:12 +0530

    fix mongodb connection before server start

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL, {})
+    return mongoose.connect(process.env.DATABASE_URL, {})
     .then(() => console.log("DB connection established successfully!"))
     .catch((err) => console.log("Something went wrong while connecting to DB",err))
 }
